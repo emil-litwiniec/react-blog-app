ef4127c17208a61690e5e309fc50b19a04179b82
Emil <emilitwiniec@gmail.com>
2019-04-19 12:23:30 +0200

    Form styling and adding scroll if it extends out of the viewport

--- src/components/PostEditPage.js
+++ src/components/PostEditPage.js
@@ -3,6 +3,10 @@ import { connect } from "react-redux";
 import PostForm from "./PostForm";
 import { startEditPost } from "../actions/posts";
 
+import SimpleBar from 'simplebar-react';
+
+import 'simplebar/dist/simplebar.min.css';
+
 export class PostEditPage extends Component {
     onSubmit = (post) => {
         console.log('hello from onSubmit')
@@ -16,14 +20,16 @@ export class PostEditPage extends Component {
         return (
 
 
-            <div className="create-post">
+            <SimpleBar>
+                <div className="create-post">
+                    <header className="create-post__header">
+                        <h2 className="create-post__title" >Edit post</h2>
+                        {/* <button className="button create-post__button">Login</button> */}
+                    </header>
+                    <PostForm onSubmit={this.onSubmit} onAbort={this.onAbort} post={this.props.post} />
 
-                <header className="create-post__header">
-                    <h2 className="create-post__title" >Edit post</h2>
-                    {/* <button className="button create-post__button">Login</button> */}
-                </header>
-                <PostForm onSubmit={this.onSubmit} onAbort={this.onAbort} post={this.props.post} />
-            </div>
+                </div>
+            </SimpleBar>
         )
     }
 }
