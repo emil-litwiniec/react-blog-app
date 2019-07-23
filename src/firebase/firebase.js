ee2eebe581f121c1134188d2f69255919f2730e7
Emil Litwiniec <emilitwiniec@gmail.com>
2019-07-23 22:43:12 +0200

    Change to webpack config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,15 @@
 import * as firebase from "firebase";
 
 const config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+    apiKey: "AIzaSyAvPy-awuBVwYJZ327qtI4Hcpt6jFjz_bQ",
+    authDomain: "blog-app-by-emil.firebaseapp.com",
+    databaseURL: "https://blog-app-by-emil.firebaseio.com",
+    projectId: "blog-app-by-emil",
+    storageBucket: "blog-app-by-emil.appspot.com",
+    messagingSenderId: "740087978540"
 };
+
+console.log(config);
 
 
 firebase.initializeApp(config);
