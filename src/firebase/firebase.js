0274305430ecb1fbb486de8789911af754ab3ee3
Emil Litwiniec <emilitwiniec@gmail.com>
2019-07-23 23:22:46 +0200

    Change in config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,13 @@
 import * as firebase from "firebase";
 
 const config = {
-    apiKey: "AIzaSyAvPy-awuBVwYJZ327qtI4Hcpt6jFjz_bQ",
-    authDomain: "blog-app-by-emil.firebaseapp.com",
-    databaseURL: "https://blog-app-by-emil.firebaseio.com",
-    projectId: "blog-app-by-emil",
-    storageBucket: "blog-app-by-emil.appspot.com",
-    messagingSenderId: "740087978540"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
-
-console.log(process.env.FIREBASE_API_KEY);
 
 
 firebase.initializeApp(config);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const dotenv = require('dotenv');
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
@@ -15,11 +14,7 @@ if (process.env.NODE_ENV === "test") {
 module.exports = env => {
     const isProduction = env === "production";
     const CSSExtract = new ExtractTextPlugin("styles.css");
-    const env2 = dotenv.config().parsed;
-    const envKeys = Object.keys(env2).reduce((prev, next) => {
-        prev[`process.env.${next}`] = JSON.stringify(env2[next]);
-        return prev;
-      }, {});
+   
     return {
         entry: ['babel-polyfill', "./src/app.js"],
         output: {
@@ -56,7 +51,26 @@ module.exports = env => {
         },
         plugins: [
             CSSExtract,
-            new webpack.DefinePlugin(envKeys)
+            new webpack.DefinePlugin({
+                "process.env.FIREBASE_API_KEY": JSON.stringify(
+                    process.env.FIREBASE_API_KEY
+                ),
+                "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(
+                    process.env.FIREBASE_AUTH_DOMAIN
+                ),
+                "process.env.FIREBASE_DATABASE_URL": JSON.stringify(
+                    process.env.FIREBASE_DATABASE_URL
+                ),
+                "process.env.FIREBASE_PROJECT_ID": JSON.stringify(
+                    process.env.FIREBASE_PROJECT_ID
+                ),
+                "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(
+                    process.env.FIREBASE_STORAGE_BUCKET
+                ),
+                "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(
+                    process.env.FIREBASE_MESSAGING_SENDER_ID
+                )
+            })
         ],
         devtool: isProduction ? "source-map" : "inline-source-map",
         devServer: {
