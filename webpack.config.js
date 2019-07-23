76e27cecc9576fb10068eb251718b94b87d218a8
Emil Litwiniec <emilitwiniec@gmail.com>
2019-07-23 23:29:17 +0200

    Change config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-
 if (process.env.NODE_ENV === "test") {
     require("dotenv").config({ path: ".env.test" });
 } else if (process.env.NODE_ENV === "development") {
