20ed8d0c36304b0ea0eae6ac3050908d9f3eed0d
Emil Litwiniec <emilitwiniec@gmail.com>
2019-07-26 21:52:57 +0200

    Update min-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require("mini-css-extract-plugin");
 const CompressionPlugin = require('compression-webpack-plugin');
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 
